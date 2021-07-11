14339b0bd4390585e8dc240248eb031417e521ff
Iagor Moraes <iagor2014@outlook.com>
2021-07-10 21:42:43 -0300

    refactor: removed api keys
    - added note

---
+++ src/config/marvel.js
@@ -0,0 +1,7 @@
+const marvelConfig = {
+  // uncomment after added the keys
+  // MARVEL_PUBLIC_KEY: 'PUBLIC_KEY here',
+  // MARVEL_PRIVATE_KEY: 'PRIVATE KEY here',
+}
+
+export default marvelConfig
